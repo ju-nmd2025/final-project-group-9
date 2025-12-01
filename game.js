f75256523871ed77e8453dba89ae8596154b640d
lucakele17 <lucakele@gmail.com>
2025-12-01 16:39:40 +0100

    Platform moving and added coloring functionality

--- game.js
+++ game.js
@@ -3,6 +3,9 @@ import { createPlatform } from "./platform";
 import { createSpike } from "./spike";
 
 let character = createCharacter(50, 50, 50, 50);
+let normalPlatform = createPlatform(70, 38, 125, 20, 0, 0, 102);
+let movingPlatform = createPlatform(225, 175, 100, 20, 0, 0, 102);
+let breakingPlatform = createPlatform(26, 270, 100, 20, 0, 0, 102);
 
 function setup() {
   createCanvas(400, 400);
@@ -23,24 +26,26 @@ function draw() {
   background(100, 100, 100);
 
   character.draw();
-  const normalPlatform1 = createPlatform(70, 38, 125, 20).draw();
-  const normalPlatform2 = createPlatform(70, 68, 125, 18).draw();
-  const normalPlatform3 = createPlatform(70, 98, 125, 20).draw();
+  normalPlatform.draw();
+  breakingPlatform.draw();
+  movingPlatform.draw();
 
-  const movingPlatform1 = createPlatform(225, 175, 100, 20).draw();
-  const movingPlatform2 = createPlatform(225, 205, 100, 20).draw();
-  const movingPlatform3 = createPlatform(225, 235, 100, 20).draw();
-
-  const breakingPlatform1 = createPlatform(26, 270, 100, 20).draw();
-  const breakingPlatform2 = createPlatform(26, 300, 100, 20).draw();
-  const breakingPlatform4 = createPlatform(26, 330, 100, 20).draw();
-  //platform.draw();
   createSpike(180, 300, 210, 240, 240, 300).draw();
 
-  /*platform.x -= 10;
-    if(platform.x + platform.w < 0){
-        platform.x = 500;
-    }*/
+  normalPlatform.x -= 10;
+  if (normalPlatform.x + normalPlatform.w < 0) {
+    normalPlatform.x = 500;
+  }
+
+  breakingPlatform.x -= 10;
+  if (breakingPlatform.x + breakingPlatform.w < 0) {
+    breakingPlatform.x = 500;
+  }
+
+  movingPlatform.x -= 10;
+  if (movingPlatform.x + movingPlatform.w < 0) {
+    movingPlatform.x = 500;
+  }
 
   if (character.y + character.h < 320) {
     character.y += 10;
