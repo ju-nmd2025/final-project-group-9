efa1529f175583e7b7c1d9566ae1d373c5bf2a10
bernadettboldizsar-droid <bernadettboldizsar@gmail.com>
2025-12-05 16:11:12 +0100

    Classified objects
    objects sorted into classes instad of functions

--- character.js
+++ character.js
@@ -1,15 +1,13 @@
-export function createCharacter(x, y, w, h) {
-  let character = {};
-  character.x = x;
-  character.y = y;
-  character.w = w;
-  character.h = h;
-
-  character.draw = function () {
-    //Character
+export default class Character {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+  draw() {
     fill(255, 204, 0);
     noStroke();
     ellipse(this.x, this.y, this.w, this.h);
-  };
-  return character;
+  }
 }
--- platform.js
+++ platform.js
@@ -1,18 +1,14 @@
-export function createPlatform(x, y, w, h, r, g, b) {
-  let platform = {};
-  platform.x = x;
-  platform.y = y;
-  platform.w = w;
-  platform.h = h;
-  platform.r = r;
-  platform.g = g;
-  platform.b = b;
-
-  platform.draw = function () {
+export default class Platform {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+  draw() {
     push();
-    fill(this.r, this.g, this.b);
+    fill(0, 0, 102);
     rect(this.x, this.y, this.w, this.h);
     pop();
-  };
-  return platform;
+  }
 }
