5454d6f7379b6a6aa724f41f21c2a5c37d366bef
lucakele17 <lucakele@gmail.com>
2025-12-13 20:07:24 +0100

    Refactor the platform movement and collisions

--- platform.js
+++ platform.js
@@ -1,19 +1,27 @@
 export default class Platform {
-  constructor(x, y, w, h, breakable) {
+  constructor(x, y, w, h, breakable, numberOfJumps) {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
     this.breakable = breakable;
+    this.numberOfJumps = numberOfJumps;
   }
   draw() {
     push();
-    fill(211, 211, 211);
+    if (this.breakable && this.numberOfJumps > 1) {
+      fill("red");
+    } else {
+      fill(211, 211, 211);
+    }
     rect(this.x, this.y, this.w, this.h);
     pop();
   }
   move(speed) {
-    this.x -= speed;
+    this.y += speed;
+  }
+  breakPlatform() {
+    return this.breakable && this.numberOfJumps == 2;
   }
 }
 
